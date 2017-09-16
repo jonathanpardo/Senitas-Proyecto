0261ab9e2ab296d27a2c4d97dd2c5aa10629055e
andresjam97 <iacastro94@misena.edu.co>
2017-09-16 14:31:12 -0500

    Cadena de Conexion nube

--- Proyecto/php/Conexion.php
+++ Proyecto/php/Conexion.php
@@ -5,7 +5,7 @@
 		{
 			try
 			{		
-		    	$Conexion = new PDO('mysql:host=localhost; dbname=senitas','root','');
+		    	$Conexion = new PDO('mysql:host=localhost; dbname=id2759632_senitas','id2759632_senitas','123456');
 		    	$Conexion->exec("SET CHARSET SET utf8");
 			}			
 			catch (Exception $e)
